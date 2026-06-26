e041bdcac2dc575d456e6d36343b294d9d717899
Martin Rodriguez <rodriguezb.martin@gmail.com>
2026-06-25 22:20:49 -0300

    chore(deploy): artefactos build [auto]

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
 //  - Bypass: analytics, dominios externos (gtag, fonts.googleapis con request por separado)
 //  - Versionado por CACHE_VERSION: cambiar el número fuerza re-cache en deploy
 
-const CACHE_VERSION = 'hc-1782435757058';
+const CACHE_VERSION = 'hc-1782436417141';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGES_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
