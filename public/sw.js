62a364eea2c60d1b3c1f479d775e940c50af4a92
Martin Rodriguez <rodriguezb.martin@gmail.com>
2026-07-28 16:49:30 -0300

    chore(deploy): cambios pre-build [auto]

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
 //  - Bypass: analytics, dominios externos (gtag, fonts.googleapis con request por separado)
 //  - Versionado por CACHE_VERSION: cambiar el número fuerza re-cache en deploy
 
-const CACHE_VERSION = 'hc-1785267720954';
+const CACHE_VERSION = 'hc-1785268014961';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGES_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
