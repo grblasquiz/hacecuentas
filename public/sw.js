57695fa93ca632e10a71bd4d8290095240ac9f7c
Martin Rodriguez <marrod@MacBook-Air-de-Martin.local>
2026-08-19 12:25:31 -0300

    chore(deploy): artefactos build [auto]

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
 //  - Bypass: analytics, dominios externos (gtag, fonts.googleapis con request por separado)
 //  - Versionado por CACHE_VERSION: cambiar el número fuerza re-cache en deploy
 
-const CACHE_VERSION = 'hc-1787150978202';
+const CACHE_VERSION = 'hc-1787152976005';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGES_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
